eee53ca12e12d78b7edc6f1e30a6fa99b9e9e19e
Sviatoslav Naiden <sv.nayden@gmail.com>
2018-05-21 21:58:15 +0300

    paths -> url

--- digitallab/urls.py
+++ digitallab/urls.py
@@ -1,17 +1,17 @@
-from django.urls import path
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'digitallab'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='digitallab/index.html')),
-    path('compounds/view-compounds', views.CompoundView.as_view()),
-    path('compounds/edit-compound', views.edit_compound),
-    path('compounds/save-compound', views.save_compound),
-    path('reagents/view-reagents', views.ReagentsView.as_view()),
-    path('reagents/add-reagent', views.edit_reagents),
-    path('reagentlocations/view-reagentlocations', views.reagent_locations),
-    path('reagentlocations/save', views.reagent_locations),
-    path('reagentlocations/add-reagentlocation', views.edit_reagent_location),
+    url(r'^$', TemplateView.as_view(template_name='digitallab/index.html')),
+    url(r'^compounds/view-compounds$', views.CompoundView.as_view()),
+    url(r'^compounds/edit-compound$', views.edit_compound),
+    url(r'^compounds/save-compound$', views.save_compound),
+    url(r'^reagents/view-reagents$', views.ReagentsView.as_view()),
+    url(r'^reagents/add-reagent$', views.edit_reagents),
+    url(r'^reagentlocations/view-reagentlocations$', views.reagent_locations),
+    url(r'^reagentlocations/save$', views.reagent_locations),
+    url(r'^reagentlocations/add-reagentlocation$', views.edit_reagent_location),
 ]
--- test_django/urls.py
+++ test_django/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.conf.urls import url
+
 
 urlpatterns = [
-    path('digitallab/', include('digitallab.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^digitallab/', include('digitallab.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
