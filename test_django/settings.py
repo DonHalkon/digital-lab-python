97d9f30ccfaa1a89b6d2e12658464269f25bbd88
Sviatoslav Naiden <sv.nayden@gmail.com>
2018-05-24 20:53:38 +0300

    Update settings.py

--- test_django/settings.py
+++ test_django/settings.py
@@ -14,16 +14,16 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DIGITAL_LAB_TEMPLATES_DIR = os.path.join(BASE_DIR, 'digitallab/templates')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, STATIC_URL[1:]),
+    STATIC_URL, 
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL[1:])
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
